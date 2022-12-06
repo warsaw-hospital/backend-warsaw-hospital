53cf6d18d3e26e5e78ff4b94fa85da8b8170e46e
KerniusSur <kernius.survila@gmail.com>
2022-12-06 14:54:22 +0200

    WHO-6 doctor & appointment specifications

--- src/main/java/com/warsaw/hospital/appointment/specifications/AppointmentSpecifications.java
+++ src/main/java/com/warsaw/hospital/appointment/specifications/AppointmentSpecifications.java
@@ -5,11 +5,35 @@ import com.warsaw.hospital.appointment.entity.enums.AppointmentStatusEnum;
 import io.micrometer.core.lang.Nullable;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppointmentSpecifications {
+  public static Specification<AppointmentEntity> hasEqualDoctorId(Long doctorId) {
+    return (root, query, cb) -> cb.equal(root.join("doctor").get("id"), doctorId);
+  }
+
+  public static Specification<AppointmentEntity> hasEqualPatientId(Long patientId) {
+    return (root, query, cb) -> cb.equal(root.join("patient").get("id"), patientId);
+  }
+
+  public static Specification<AppointmentEntity> hasEqualStatus(AppointmentStatusEnum status) {
+    return (root, query, cb) -> cb.equal(root.get("status"), status);
+  }
+
   public static List<Specification<AppointmentEntity>> getSpecifications(
-      @Nullable AppointmentStatusEnum status, @Nullable Long userId, @Nullable Long doctorId) {
-    return List.of();
+      @Nullable AppointmentStatusEnum status, @Nullable Long patientId, @Nullable Long doctorId) {
+    List<Specification<AppointmentEntity>> specifications = new ArrayList<>();
+    if (status == null && patientId == null && doctorId == null) {
+      specifications.add(hasEqualDoctorId(doctorId));
+    }
+    if (status != null) {
+      specifications.add(hasEqualPatientId(patientId));
+    }
+    if (status != null) {
+      specifications.add(hasEqualStatus(status));
+    }
+
+    return specifications;
   }
 }
--- src/main/java/com/warsaw/hospital/doctor/specifications/DoctorSpecifications.java
+++ src/main/java/com/warsaw/hospital/doctor/specifications/DoctorSpecifications.java
@@ -5,12 +5,41 @@ import com.warsaw.hospital.doctor.enums.DoctorSpecializationEnum;
 import io.micrometer.core.lang.Nullable;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DoctorSpecifications {
-  //TODO: add more specifications
+
+  public static Specification<DoctorEntity> filterBySearch(String search) {
+    return (root, query, cb) -> {
+      List<Predicate> predicates = new ArrayList<>();
+      List<String> words = Arrays.stream(search.split(" ")).map(word -> "%" + word + "%").toList();
+      for (String word : words) {
+        predicates.add(cb.like(cb.lower(root.get("name")), word.toLowerCase()));
+        predicates.add(cb.like(cb.lower(root.get("surname")), word.toLowerCase()));
+        predicates.add(cb.like(cb.lower(root.get("email")), word.toLowerCase()));
+        predicates.add(cb.like(root.get("phoneNumber"), word));
+      }
+      return cb.or(predicates.toArray(new Predicate[0]));
+    };
+  }
+
+  public static Specification<DoctorEntity> hasEqualSpecialization(
+      DoctorSpecializationEnum specialization) {
+    return (root, query, cb) -> cb.equal(root.get("specialization"), specialization);
+  }
+
   public static List<Specification<DoctorEntity>> getSpecifications(
       @Nullable String search, @Nullable DoctorSpecializationEnum specialization) {
-    return List.of();
+    List<Specification<DoctorEntity>> specifications = new ArrayList<>();
+    if (search != null) {
+      specifications.add(filterBySearch(search));
+    }
+    if (specialization != null) {
+      specifications.add(hasEqualSpecialization(specialization));
+    }
+    return specifications;
   }
 }
